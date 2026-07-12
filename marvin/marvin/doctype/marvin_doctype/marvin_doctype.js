711a74dab1f5e729f5b7f97afeff557d92a0cae4
Marvin <dizormarvin@gmail.com>
2026-07-12 11:38:48 +0800

    Update marvin_doctype.js

--- marvin/marvin/doctype/marvin_doctype/marvin_doctype.js
+++ marvin/marvin/doctype/marvin_doctype/marvin_doctype.js
@@ -1,8 +1,9 @@
 // Copyright (c) 2026, Marvin and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Marvin Doctype", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Marvin Doctype", {
+	refresh(frm) {
+        frappe.throw("test");
+        
+	},
+});
