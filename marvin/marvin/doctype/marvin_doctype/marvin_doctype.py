6b0eb6b3fba23e000880a1773fcd85408605c5f5
Marvin <dizormarvin@gmail.com>
2026-07-12 21:05:58 +0800

    Update marvin_doctype.py

--- marvin/marvin/doctype/marvin_doctype/marvin_doctype.py
+++ marvin/marvin/doctype/marvin_doctype/marvin_doctype.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2026, Marvin and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class MarvinDoctype(Document):
-	pass
+	def validate(self):
+		frappe.throw('test')
